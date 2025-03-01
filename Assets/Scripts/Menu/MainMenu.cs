2d2b3182b83241857c9f01b063fbb3c9791bb4ed
Victor <victor.tendron@epita.fr>
2025-03-01 17:22:45 +0100

    Added MainMenu, Multiplayer Menu

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject mainMenuCanvas;
+    public GameObject multiplayerMenuCanvas;
+
+    public void ShowMultiplayerMenu()
+    {
+        mainMenuCanvas.SetActive(false);
+        multiplayerMenuCanvas.SetActive(true);
+    }
+
+    public void ShowMainMenu()
+    {
+        multiplayerMenuCanvas.SetActive(false);
+        mainMenuCanvas.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
