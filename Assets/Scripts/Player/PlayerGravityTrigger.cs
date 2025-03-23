340724ef1c019fab5b1fefb8fd6628e0e0095454
lomination <lomination@rejna.org>
2025-03-23 18:21:47 +0100

    Fix scripts

--- Assets/Scripts/Player/PlayerGravityTrigger.cs
+++ Assets/Scripts/Player/PlayerGravityTrigger.cs
@@ -3,16 +3,16 @@ using Player;
 using Reconnect.Physics;
 using UnityEngine;
 
-public class PlayerGraviityTrigger : MonoBehaviour
+public class PlayerGravityTrigger : MonoBehaviour
 {
-    private void OnCollisionExit(Collision collision)
+    private void OnTriggerExit(Collider other)
     {
-        if (collision.gameObject.CompareTag("AirLock"))
+        if (other.gameObject.CompareTag("AirLock"))
         {
             Debug.Log("Player collided with the AirLock layer!");
             if (!TryGetComponent(out PhysicsScript physicsScript))
                 throw new ArgumentException("No PhysicsScript found on this Player");
-            if (!collision.gameObject.TryGetComponent(out IAirlockCollider airlockCollider))
+            if (!other.gameObject.TryGetComponent(out IAirlockCollider airlockCollider))
                 throw new ArgumentException("No Airlock collider found on this AirLock tagged object");
             airlockCollider.CollisionHandle(physicsScript);
         }
