a049d5aa54dc2334e748bfb8112b185b4f54e584
Nota-yob <142342012+Nota-yob@users.noreply.github.com>
2025-03-26 12:56:05 +0100

    fix #29
    - Series merge node selection edit to change the common one
    - Series merge recursion fixed : merge into branch to add

--- Assets/Scripts/Electronics/UI/CircuitSubmitButton.cs
+++ Assets/Scripts/Electronics/UI/CircuitSubmitButton.cs
@@ -14,19 +14,26 @@ namespace Reconnect.Electronics.UI
         {
             BreadboardUI = GetComponentInParent<BreadboardUI>();
         }
-
+        
         public void ExecuteCircuit()
         {
             Graph circuitGraph = BreadboardUI.Breadboard.CreateGraph();
-            Debug.Log($"STATE :::\n"+string.Join('\n', from v in circuitGraph.Vertices select $"{v.GetType().Name[..3]} {v.Name}: [{string.Join(", ", v.AdjacentComponents)}]"));
+            //Debug.Log($"STATE :::\n"+string.Join('\n', from v in circuitGraph.Vertices select $"{v.GetType().Name[..3]} {v.Name}: [{string.Join(", ", v.AdjacentComponents)}]"));
             circuitGraph.DefineBranches();
-            Debug.Log($"VERTICES({circuitGraph.Vertices.Count}) :::\n"+string.Join('\n', circuitGraph.Vertices));
-            Debug.Log($"BRANCHES({circuitGraph.Branches.Count}) :::\n"+string.Join('\n', circuitGraph.Branches));
+            //Debug.Log($"VERTICES({circuitGraph.Vertices.Count}) :::\n"+string.Join('\n', circuitGraph.Vertices));
+            string branchesDebug = "";
+            foreach (Branch circuitGraphBranch in circuitGraph.Branches)
+            {
+                branchesDebug += circuitGraphBranch.Display() + '\n';
+            }
+            //Debug.Log($"BRANCHES({circuitGraph.Branches.Count}) :::\n"+branchesDebug);
+            //Debug.Log($"BRANCHES({circuitGraph.Branches.Count}) :::\n"+string.Join('\n', circuitGraph.Branches));
             double intensity = circuitGraph.GetGlobalIntensity();
+            //Debug.Log($"INSENTITY ::: {intensity} A");
             Lamp targetLamp = (Lamp) BreadboardUI.Breadboard.Target;
             targetLamp.Set(intensity);
-            Debug.Log(targetLamp.isLampOn(intensity) ? "The lamp is ON ! Success" : "The lamp is OFF ! You failed");
-            Debug.Log($"tension of the target : {targetLamp.GetVoltage(intensity)} Volts");
+            //Debug.Log(targetLamp.isLampOn(intensity) ? "The lamp is ON ! Success" : "The lamp is OFF ! You failed");
+            //Debug.Log($"tension of the target : {targetLamp.GetVoltage(intensity)} Volts");
         }
     }
 }
