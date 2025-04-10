b8998a8d1e4365b6294fdc343d521c330f4edb70
Nota-yob <142342012+Nota-yob@users.noreply.github.com>
2025-04-10 02:25:25 +0200

    breadboard prefab, circuit instructions, video player
    Video of rickroll for testing purpose, sould be replaced later by video to display

---
+++ Assets/Scripts/Electronics/UI/CircuitInstructions.cs
@@ -0,0 +1,38 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class CircuitInstructions : MonoBehaviour
+{
+    [SerializeField]
+    private string title;
+    [TextArea(5,15)] [SerializeField]
+    private string instructions;
+    [TextArea(3,8)] [SerializeField]
+    private string footer;
+
+    [SerializeField]
+    private TextMeshProUGUI titleMesh;
+    [SerializeField]
+    private TextMeshProUGUI instructionMesh;
+    [SerializeField]
+    private TextMeshProUGUI footerMesh;
+
+    [SerializeField] private GameObject leftPanel;
+    [SerializeField] private GameObject rightPanel;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        titleMesh.text = title;
+        instructionMesh.text = instructions;
+        footerMesh.text = footer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UI/VideoController.cs
@@ -0,0 +1,31 @@
+using UnityEditor.Build;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public GameObject videoScreen; // The RawImage object
+    public void PlayVideo()
+    {
+        videoScreen.SetActive(true);
+        videoPlayer.Play();
+    }
+
+    public void StopVideo()
+    {
+        videoPlayer.Stop();
+        videoScreen.SetActive(false);
+    }
+    
+    public void PlayPauseVideo()
+    {
+        if (videoPlayer.isPlaying)
+            videoPlayer.Pause();
+        else
+        {
+            PlayVideo();
+        }
+        
+    }
+}
