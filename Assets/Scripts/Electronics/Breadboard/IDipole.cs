d75392356f424108e5b78c315ca4d38dfb588aef
lomination <lomination@rejna.org>
2025-04-27 00:58:00 +0200

    Make GetFlattenedCursorPos relative

--- Assets/Scripts/Electronics/Breadboard/IDipole.cs
+++ Assets/Scripts/Electronics/Breadboard/IDipole.cs
@@ -6,7 +6,7 @@ namespace Reconnect.Electronics.Breadboards
     public interface IDipole
     {
         public Breadboard Breadboard { get; set; }
-        public Vector2Int Pole1 { get;set; }
-        public Vector2Int Pole2 { get;set; } 
+        public Vector2Int Pole1 { get; set; }
+        public Vector2Int Pole2 { get; set; } 
     }
 }
