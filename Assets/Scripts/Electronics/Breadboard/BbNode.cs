3011f93949f8953031b877898be00dea384fa8a2
lomination <lomination@rejna.org>
2025-04-28 00:23:28 +0200

    Make outline on nodes and refactor the prefabs

--- Assets/Scripts/Electronics/Breadboard/BbNode.cs
+++ Assets/Scripts/Electronics/Breadboard/BbNode.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -13,16 +14,18 @@ namespace Reconnect.Electronics.Breadboards
         [SerializeField]
         private Breadboard breadboard;
 
-        private void OnMouseDown()
+        private Outline _outline;
+
+        private void Start()
         {
-            // The click is over a UI element with Raycast Target = true
-            if (EventSystem.current != null && EventSystem.current.IsPointerOverGameObject())
-                return;
-            breadboard.StartWire(point);
+            _outline = GetComponent<Outline>();
+            _outline.enabled = false;
         }
 
         private void OnMouseEnter()
         {
+            _outline.enabled = true;
+
             if (IsPointerOverUI())
             {
                 breadboard.EndWire();
@@ -33,6 +36,19 @@ namespace Reconnect.Electronics.Breadboards
             }
         }
 
+        private void OnMouseExit()
+        {
+            _outline.enabled = false;
+        }
+
+        private void OnMouseDown()
+        {
+            // The click is over a UI element with Raycast Target = true
+            if (EventSystem.current != null && EventSystem.current.IsPointerOverGameObject())
+                return;
+            breadboard.StartWire(point);
+        }
+        
         private void OnMouseUp()
         {
             breadboard.EndWire();
