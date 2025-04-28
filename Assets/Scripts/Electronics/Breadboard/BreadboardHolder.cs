8bd8e1c23bde7332004f0b0c2197ba5bdc76cee2
lomination <lomination@rejna.org>
2025-04-28 14:18:57 +0200

    Make the breadboard holder an interactable

--- Assets/Scripts/Electronics/Breadboard/BreadboardHolder.cs
+++ Assets/Scripts/Electronics/Breadboard/BreadboardHolder.cs
@@ -1,4 +1,7 @@
 using System;
+using Reconnect.Interactions;
+using Reconnect.Player;
+using Unity.Cinemachine;
 using UnityEngine;
 
 namespace Reconnect.Electronics.Breadboards
@@ -6,15 +9,60 @@ namespace Reconnect.Electronics.Breadboards
     /// <summary>
     /// To ease the access to the breadboard, the UI and the camera. 
     /// </summary>
-    public class BreadboardHolder : MonoBehaviour
+    public class BreadboardHolder : Interactable
     {
         public Breadboard breadboard;
-        public Camera cam;
+        public CinemachineCamera cam;
+        public GameObject ui;
+        private bool _isActive = false;
+        
+        private new void Start()
+        {
+            base.Start();
+            cam.gameObject.SetActive(false);
+            cam.Priority = 0;
+            ui.SetActive(false);
+        }
+
+        public override void Interact(GameObject player)
+        {
+            if (_isActive)
+            {
+                // quit the interface
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+                Outline.enabled = true;
+                player.GetComponent<PlayerMovementsNetwork>().isLocked = false;
+                player.transform.GetChild(0).gameObject.SetActive(true);
+                cam.gameObject.SetActive(false);
+                cam.Priority = 0;
+                ui.SetActive(false);
+                _isActive = false;
+            }
+            else
+            {
+                // enter the interface
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.Confined;
+                Outline.enabled = false;
+                player.GetComponent<PlayerMovementsNetwork>().isLocked = true;
+                player.transform.GetChild(0).gameObject.SetActive(false);
+                cam.gameObject.SetActive(true);
+                cam.Priority = 2;
+                ui.SetActive(true);
+                _isActive = true;
+            }
+        }
+
+        public override bool CanInteract()
+        {
+            return true;
+        }
         
         public Vector3 GetFlattenedCursorPos()
         {
             Plane plane = new Plane(transform.rotation * Vector3.forward, transform.position);
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            Ray ray = Camera.main!.ScreenPointToRay(Input.mousePosition);
 
             if (plane.Raycast(ray, out var dist))
                 return ray.GetPoint(dist);
--- Assets/Scripts/Scene/SceneScript.cs
+++ Assets/Scripts/Scene/SceneScript.cs
@@ -1,28 +1,28 @@
-using Mirror;
-using Reconnect.Player;
-using UnityEngine.Serialization;
-using UnityEngine.UI;
-
-namespace Reconnect.Scene
-{
-    public class SceneScript : NetworkBehaviour
-    {
-        public Text canvasStatusText;
-        [FormerlySerializedAs("playerScript")] public PlayerNetwork playerNetwork;
-
-        [SyncVar(hook = nameof(OnStatusTextChanged))]
-        public string statusText;
-
-        private void OnStatusTextChanged(string _Old, string _New)
-        {
-            //called from sync var hook, to update info on screen for all players
-            canvasStatusText.text = statusText;
-        }
-
-        public void ButtonSendMessage()
-        {
-            if (playerNetwork != null)
-                playerNetwork.CmdSendPlayerMessage();
-        }
-    }
-}
+// using Mirror;
+// using Reconnect.Player;
+// using UnityEngine.Serialization;
+// using UnityEngine.UI;
+//
+// namespace Reconnect.Scene
+// {
+//     public class SceneScript : NetworkBehaviour
+//     {
+//         public Text canvasStatusText;
+//         [FormerlySerializedAs("playerScript")] public PlayerNetwork playerNetwork;
+//
+//         [SyncVar(hook = nameof(OnStatusTextChanged))]
+//         public string statusText;
+//
+//         private void OnStatusTextChanged(string _Old, string _New)
+//         {
+//             //called from sync var hook, to update info on screen for all players
+//             canvasStatusText.text = statusText;
+//         }
+//
+//         public void ButtonSendMessage()
+//         {
+//             if (playerNetwork != null)
+//                 playerNetwork.CmdSendPlayerMessage();
+//         }
+//     }
+// }
