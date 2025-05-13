f55ef91ae3340fdf94949b2ad463265f0f2ff88a
Nota-yob <142342012+Nota-yob@users.noreply.github.com>
2025-05-14 01:30:15 +0200

    add a pause system + change speed on attack mode

--- Assets/Scripts/Pathfinding/PassiveMob.cs
+++ Assets/Scripts/Pathfinding/PassiveMob.cs
@@ -1,5 +1,4 @@
-using System.Collections.Generic;
-using System.Linq;
+using System.Collections;
 using Reconnect.Utils;
 using UnityEngine;
 using UnityEngine.AI;
@@ -9,18 +8,36 @@ namespace Reconnect.Pathfinding
 {
     public class PassiveMob : MonoBehaviour
     {
-        private NavMeshAgent _agent; 
+        [SerializeField] private float maxWaitingTime = 15f;
+        [SerializeField] private float minWaitingTime = 5f;
+        private NavMeshAgent _agent;
+        private bool _isWaiting;
         void Start()
         {
             if (!TryGetComponent(out _agent))
                 throw new ComponentNotFoundException("No NavMeshAgent component has been found on this mob.");
+            SetDestination();
         }
+
+        private IEnumerator PauseForSeconds(float seconds)
+        {
+            _isWaiting = true;
+            yield return new WaitForSeconds(seconds);
+            _isWaiting = false;
+            SetDestination();
+        }
+
+        private void SetDestination()
+        {
+            _agent.SetDestination(transform.position + new Vector3(Random.Range(-30, 30), 0, Random.Range(-30, 30)));
+        }
+        
         void Update()
         {
-            if (!_agent.hasPath || _agent.remainingDistance <= 3)
+            if (_agent.remainingDistance <= 3 && !_isWaiting)
             {
-                _agent.SetDestination(transform.position + new Vector3(Random.Range(-30, 30), 0, Random.Range(-30, 30)));
-            }  
+                StartCoroutine(PauseForSeconds(Random.Range(minWaitingTime, maxWaitingTime)));
+            }
         }
     }   
 }
