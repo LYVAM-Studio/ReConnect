d255d0562f2a9501be070c552a540021ceea0c93
Nota-yob <142342012+Nota-yob@users.noreply.github.com>
2025-05-14 13:46:11 +0200

    add cheat code scripts for testing purposes

---
+++ Assets/Scripts/CheatCodes/CheatCodes.cs
@@ -0,0 +1,55 @@
+using Mirror;
+using Reconnect;
+using Reconnect.Menu;
+using Reconnect.Menu.Lessons;
+using Reconnect.Player;
+using Reconnect.Utils;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Reconnect.CheatCodes
+{
+    public class CheatCodes : MonoBehaviour
+    {
+
+        private PlayerControls _controls;
+        private TestLessons _testLessons;
+        private void Awake()
+        {
+            if (!TryGetComponent(out _testLessons))
+                throw new ComponentNotFoundException("No TestLessons component has been found on the CheatCodeManager");
+            _controls = new PlayerControls();
+            _controls.CheatCodes.KnockOut.performed += OnKnockOut;
+            _controls.CheatCodes.PopulateLessons.performed += OnPopulateLessons;
+        }
+        
+        private void OnEnable()
+        {
+            _controls.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _controls.Disable();
+        }
+
+        private void OnDestroy()
+        {
+            _controls.CheatCodes.KnockOut.performed -= OnKnockOut;
+            _controls.CheatCodes.PopulateLessons.performed += OnPopulateLessons;
+        }
+
+        public void OnKnockOut(InputAction.CallbackContext ctx)
+        {
+            if (!NetworkClient.localPlayer.gameObject.TryGetComponent<PlayerMovementsNetwork>(out PlayerMovementsNetwork playerMovements))
+                throw new ComponentNotFoundException(
+                    "No PlayerMovementsNetwork found on the localPlayer gameObject");
+            playerMovements.KnockOut();
+        }
+        
+        public void OnPopulateLessons(InputAction.CallbackContext ctx)
+        {
+            _testLessons.PopulateLessons();
+        }
+    }
+}
