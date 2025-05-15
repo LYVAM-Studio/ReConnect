5c696a7f300703baebfde7dd55840f2e7e344954
max <maxime.houwenaghel@epita.fr>
2025-05-15 16:57:09 +0200

    add: animation

--- Assets/Scripts/Pathfinding/AggressiveMob.cs
+++ Assets/Scripts/Pathfinding/AggressiveMob.cs
@@ -16,18 +16,27 @@ namespace Reconnect.Pathfinding
         private List<Transform> _playersInRange = new List<Transform>();
         private bool _isWaiting;
         
+        private Animator animator;
+        private bool isRunning = false;
+        private bool isAttacking = false;
+        private bool isDying = false;
         private void Start()
         {
             if (!TryGetComponent(out _agent))
                 throw new ComponentNotFoundException("No NavMeshAgent component has been found on this mob.");
             SetDestination();
+            
+            animator = GetComponent<Animator>();
+
         }
         
         private IEnumerator PauseForSeconds(float seconds)
         {
             _isWaiting = true;
+            Exit();
             yield return new WaitForSeconds(seconds);
             _isWaiting = false;
+            Enter();
             SetDestination();
         }
 
@@ -39,16 +48,26 @@ namespace Reconnect.Pathfinding
         private void Update()
         {
             var closestPlayerTransform = GetClosestPlayerTransform();
-            if (closestPlayerTransform is not null)
+            if (closestPlayerTransform is not null && _agent.remainingDistance <= 0.5)
+            {
+                Attack();
+            }
+            else if (closestPlayerTransform is not null)
             {
                 _agent.SetDestination(closestPlayerTransform.position);
+                Run();
                 _agent.speed = 4f;
             }
             else if (_agent.remainingDistance <= 3 && !_isWaiting)
             {
                 _agent.speed = 2f;
+                Run();
                 StartCoroutine(PauseForSeconds(Random.Range(minWaitingTime, maxWaitingTime)));
             }
+            
+            animator.SetBool("IsRunning", isRunning);
+            animator.SetBool("IsAttacking", isAttacking);
+            animator.SetBool("IsDie", isDying);
         }
         
         public void OnTriggerEnter(Collider other)
@@ -73,7 +92,7 @@ namespace Reconnect.Pathfinding
                 _agent.SetDestination(transform.position + new Vector3(Random.Range(-30, 30), 0, Random.Range(-30, 30)));
             }
         }
-
+        
         private Transform GetClosestPlayerTransform()
         {
             // Remove destroyed components
@@ -84,5 +103,41 @@ namespace Reconnect.Pathfinding
                 .OrderBy(p => Vector3.Distance(transform.position, p.position))
                 .FirstOrDefault();
         }
-    }
+
+            void Enter()
+            {
+                isRunning = false;
+                isAttacking = false;
+                isDying = false;
+                animator.SetTrigger("Entry");
+            }
+
+            void Run()
+            {
+                isRunning = true;
+                isAttacking = false;
+                isDying = false;
+            }
+
+            void Attack()
+            {
+                isRunning = false;
+                isAttacking = true;
+                isDying = false;
+            }
+
+            void Die()
+            {
+                isRunning = false;
+                isAttacking = false;
+                isDying = true;
+            }
+
+            void Exit()
+            {
+                isRunning = false;
+                isAttacking = false;
+                isDying = false;
+            }
+        }
 }
