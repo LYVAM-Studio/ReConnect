00e3f0228c038b0469fe0af46724652537167367
lomination <lomination@rejna.org>
2025-05-17 21:53:43 +0200

    Wip mobs

--- Assets/Scripts/Mobs/AggressiveMob.cs
+++ Assets/Scripts/Mobs/AggressiveMob.cs
@@ -1,50 +1,27 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Reconnect.Utils;
 using UnityEngine;
-using UnityEngine.AI;
 using Random = UnityEngine.Random;
 
 namespace Reconnect.Pathfinding
 {
-    public class AggressiveMob : MonoBehaviour
+    public class AggressiveMob : Mob
     {
-        [SerializeField] private float maxWaitingTime = 15f;
-        [SerializeField] private float minWaitingTime = 5f;
-        private NavMeshAgent _agent;
         private List<Transform> _playersInRange = new List<Transform>();
-        private bool _isWaiting;
-
+        
         private Animator _animator;
-
         private int _isDeadHash;
         private int _isRunningHash;
         private int _isAttackingHash;
         
         private void Start()
         {
-            if (!TryGetComponent(out _agent))
-                throw new ComponentNotFoundException("No NavMeshAgent component has been found on this mob.");
-            SetDestination();
-
             _animator = GetComponent<Animator>();
             _isDeadHash = Animator.StringToHash("IsDead");
             _isRunningHash = Animator.StringToHash("IsRunning");
             _isAttackingHash = Animator.StringToHash("IsAttacking");
-        }
-
-        private IEnumerator PauseForSeconds(float seconds)
-        {
-            _isWaiting = true;
-            yield return new WaitForSeconds(seconds);
-            _isWaiting = false;
-            SetDestination();
-        }
-
-        private void SetDestination()
-        {
-            _agent.SetDestination(transform.position + new Vector3(Random.Range(-30, 30), 0, Random.Range(-30, 30)));
+            
+            ChooseRandomDestination();
         }
 
         private void Update()
@@ -52,29 +29,33 @@ namespace Reconnect.Pathfinding
             var closestPlayerTransform = GetClosestPlayerTransform();
             if (closestPlayerTransform is not null)
             {
-                _agent.SetDestination(closestPlayerTransform.position);
-                _agent.speed = 4f;
+                Agent.speed = 4f;
+                Agent.SetDestination(closestPlayerTransform.position);
             }
-            else if (_agent.remainingDistance <= 3 && !_isWaiting)
+            else
             {
-                StartCoroutine(PauseForSeconds(Random.Range(minWaitingTime, maxWaitingTime)));
+                Agent.speed = 2f;
+                if (Agent.remainingDistance <= 3 && !IsWaiting)
+                {
+                    StartCoroutine(PauseForSeconds(Random.Range(minWaitingTime, maxWaitingTime)));
+                }
             }
 
-            _animator.SetBool(_isRunningHash, _agent.velocity.magnitude >= 0.1f);
+            _animator.SetBool(_isRunningHash, Agent.velocity.magnitude >= 0.1f);
         }
 
         public void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player"))
             {
-                if (_isWaiting)
+                if (IsWaiting)
                 {
                     StopCoroutine(nameof(PauseForSeconds));
-                    _isWaiting = false;
+                    IsWaiting = false;
                 }
 
                 _playersInRange.Add(other.transform);
-                _agent.SetDestination(GetClosestPlayerTransform().position);
+                Agent.SetDestination(GetClosestPlayerTransform().position);
             }
         }
 
@@ -84,9 +65,9 @@ namespace Reconnect.Pathfinding
             {
                 _playersInRange.Remove(other.transform);
                 // reset the destination that will be set by update
-                _agent.SetDestination(transform.position);
+                Agent.SetDestination(transform.position);
                 // reset to low speed when not attacking player 
-                _agent.speed = 2f;
+                Agent.speed = 2f;
             }
         }
 
