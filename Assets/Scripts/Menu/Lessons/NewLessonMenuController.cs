9dba4b8d67c0f49d735b34db174de87d74a104ba
Nota-yob <142342012+Nota-yob@users.noreply.github.com>
2025-05-18 19:30:19 +0200

    add lessons corresponding to level and sync + show menu on change level

--- Assets/Scripts/Menu/Lessons/NewLessonMenuController.cs
+++ Assets/Scripts/Menu/Lessons/NewLessonMenuController.cs
@@ -1,16 +1,20 @@
 using Reconnect.Utils;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Reconnect.Menu.Lessons
 {
-    public class NewLessonImageViewer : MonoBehaviour
+    public class NewLessonMenuController : MonoBehaviour
     {
         [SerializeField] private Image image;
+        [SerializeField] private TMP_Text tmpText;
         private void Awake()
         {
             if (image is null)
                 throw new MissingSerializedFieldException(nameof(image));
+            if (tmpText is null)
+                throw new MissingSerializedFieldException(nameof(tmpText));
         }
 
         public void LoadImage(Sprite sprite)
@@ -21,6 +25,11 @@ namespace Reconnect.Menu.Lessons
         public void CloseImage()
         {
             image.sprite = null;
+        }
+        
+        public void SetTextToLevel(uint level)
+        {
+            tmpText.text = $"You are now level {level} ! Let's continue learning how electricity works together !";
         }
     }
 }
